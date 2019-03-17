0bc448e2946b5a3296006ddbd55d1d5262f27656
darling-rockstar-4852 <daisyrene483@gmail.com>
2019-03-17 19:36:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array=["Doc", "Dopey", "Bashful", "Grumpy"]
+  .include number list(1-4)
 end
 
 def summon_captain_planet# code an argument here
